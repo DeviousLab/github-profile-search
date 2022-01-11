13eefe812fcb0ccb9906c6845281ca8b424e6256
DeviousLab <deviouslab@gmail.com>
2022-01-11 20:44:23 +0400

    Created footer component
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
-        <main>
+        <main className="container mx-auto px-3 pb-12">
           content
         </main>
+        <Footer />
       </div>
     </Router>
   );
