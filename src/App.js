ecefe86dafa4806281fc08cd17eebe5891eb4739
DeviousLab <deviouslab@gmail.com>
2022-01-11 20:30:02 +0400

    Created navbar component
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main>
+          content
+        </main>
+      </div>
+    </Router>
   );
 }
 
