0db09605b214a85875471683e5aa943b84974ad2
DeviousLab <deviouslab@gmail.com>
2022-01-13 00:25:24 +0400

    Setup github context
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,26 +1,16 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useContext } from 'react';
 
 import Spinner from '../Spinner';
 import UserItem from './UserItem';
+import GithubContext from '../../context/github/GithubContext';
 
 function UserResults() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const { users, loading, fetchUsers } = useContext(GithubContext);
 
   useEffect(() => {
     fetchUsers();
   }, []);
 
-  const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_API_LINK}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_API_TOKEN}`
-      }
-    });
-    const data = await response.json();
-    setUsers(data);
-    setLoading(false);
-  }
   if (!loading) {
     return (
       <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
